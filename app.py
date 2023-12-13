f2187c5d4147dc3d9c58d724a7f03ab4672be3eb
tzuenting <a0987513294@gmail.com>
2023-12-13 22:30:08 +0800

    update for modification in main branch

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from datetime import datetime
+from datetime import date
 # import matplotlib.pyplot as plt
 # plt.rcParams['font.family'] = 'Microsoft JhengHei'
 
@@ -28,6 +28,7 @@ def GetProcessedData(file):
     # df.set_index('Date', inplace=True)
     # st.write(df.head())
     df['Date'] = pd.to_datetime(df['Date']).dt.date
+    df = df[df['Date'] >= date(2017,12,31)]
     df.sort_values(by='Date', inplace=True)
     return df
 
@@ -69,54 +70,48 @@ def CategoryTimePlot(df, selected_categories):
 # Load the data
 df = GetProcessedData('processed_data.csv')
 
-Presses = df['Press'].unique()
-Categories = ['politics','finance','entertainment','health','life','tech','global']
+media_options = df['Press'].unique().tolist()
+Category_options = ['politics','finance','entertainment','health','life','tech','global']
 
 def run():
     # Sidebar filters
     with st.sidebar:
         st.header('篩選選項')
-        selected_presses = st.multiselect('選擇媒體', Presses, default=df['Press'].unique())
-        selected_categories = st.multiselect("Select Categories", Categories, default=df['Category'].unique())
-        start_date, end_date = st.date_input('選擇日期範圍', [datetime.strptime("2018-01-01", '%Y-%m-%d'), df['Date'].max()])
-        if not start_date:
-            start_date = df['Date'].min()
-        if not end_date:
-            end_date = df['Date'].max()
+        selected_presses = st.multiselect('選擇媒體', media_options, default=df['Press'].unique())
+        selected_categories = st.multiselect("Select Categories", Category_options, default=df['Category'].unique())
+        start_date = st.date_input('開始日期', df['Date'].min())
+        end_date = st.date_input('結束日期', df['Date'].max())
     st.title('新聞標題分析 Dashboard')
     # Filter data based on selections
     # Since the category,press effect each others, move the selection into plot function
     filtered_df = SelectDate(df, start_date, end_date)
     st.subheader('篩選日期後的資料格式')
-    st.write(filtered_df.head())
+    if st.checkbox('顯示篩選後的數據'):
+        st.write(filtered_df.head())
 
     t1, t2 = st.tabs(["Pingru", "Ding"])
     with t1:
-        # mask = (df['Press'].isin(selected_press)) & (df['Date'] >= start_date) & (df['Date'] <= end_date)
-        # filtered_df = df[mask]
-
-        # Clickbait Ratio by Category Bar Chart
+        # Create and display charts with responsive layout
+        # Bar chart for Clickbait Ratio by Category
         st.header('各類別誘餌式標題比例')
-        clickbait_ratio_by_category = (
-            filtered_df.groupby('Category')['IsClickbait'].mean().reset_index()
-        )
+        clickbait_ratio_by_category = filtered_df.groupby('Category')['IsClickbait'].mean().reset_index()
         fig_clickbait_category = px.bar(clickbait_ratio_by_category, x='Category', y='IsClickbait', title='各類別誘餌式標題比例')
-        st.plotly_chart(fig_clickbait_category)
+        fig_clickbait_category.update_layout(autosize=True, margin=dict(l=20, r=20, t=20, b=20))
+        st.plotly_chart(fig_clickbait_category, use_container_width=True)
 
-        # Emotional Title Time Series Chart
-        st.header('標題情緒性隨時間變化')
-        emotional_timeseries = (
-            filtered_df.groupby('Date')['emotional'].mean().reset_index()
-        )
-        fig_emotional_timeseries = px.line(emotional_timeseries, x='Date', y='emotional', title='標題情緒性隨時間變化')
-        st.plotly_chart(fig_emotional_timeseries)
+        # Time series chart for Emotional Titles
+        st.header('情緒性標題的時間序列圖')
+        time_series_emotional = filtered_df.groupby('Date')['emotional'].mean().reset_index()
+        fig_time_series_emotional = px.line(time_series_emotional, x='Date', y='emotional', title='情緒性標題的時間趨勢')
+        fig_time_series_emotional.update_layout(autosize=True, margin=dict(l=20, r=20, t=20, b=20))
+        st.plotly_chart(fig_time_series_emotional, use_container_width=True)
 
-        st.header('不同媒體的誘餌式標題比例')
-        clickbait_ratio_by_press = (
-            filtered_df.groupby('Press')['IsClickbait'].mean().reset_index()
-        )
-        fig_clickbait_press = px.bar(clickbait_ratio_by_press, x='Press', y='IsClickbait', title='不同媒體的誘餌式標題比例')
-        st.plotly_chart(fig_clickbait_press)
+        # Bar chart for Interrogative Titles by Category
+        st.header('詢問性標題的長條圖')
+        interrogative_titles_by_category = filtered_df.groupby('Category')['interrogative'].mean().reset_index()
+        fig_interrogative_category = px.bar(interrogative_titles_by_category, x='Category', y='interrogative', title='各類別詢問性標題比例')
+        fig_interrogative_category.update_layout(autosize=True, margin=dict(l=20, r=20, t=20, b=20))
+        st.plotly_chart(fig_interrogative_category, use_container_width=True)
     with t2:
         st.header('新畫的圖')
         st.write('遇到問題:無法用plotly.express標註每條線的hover_name,color等資訊。')
