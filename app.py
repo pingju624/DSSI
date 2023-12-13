017c21683bc384a701bb6741bd9da1ab19c49467
tzuenting <a0987513294@gmail.com>
2023-12-13 21:27:02 +0800

    still didn't fix the px.line problem, yet add some page_config.

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 from datetime import datetime
-import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = 'Microsoft JhengHei'
+# import matplotlib.pyplot as plt
+# plt.rcParams['font.family'] = 'Microsoft JhengHei'
 
+st.set_page_config(
+    page_title="新聞標題分析: Clickbait",
+    page_icon="📰",
+    layout="centered",
+    initial_sidebar_state="expanded"
+)
+
+# custom_theme = """
+# [theme]
+# base="dark"
+# primaryColor="#b04bff"
+# """
 
 @st.cache_data
 def GetProcessedData(file):
     df = pd.read_csv(file)
     # Convert 'Date' column to datetime
-    df = df.iloc[:,1:]
+    # df = df.iloc[:,1:]
+    del df['Unnamed: 0']
     # df.set_index('Date', inplace=True)
-    st.write(df.head())
+    # st.write(df.head())
     df['Date'] = pd.to_datetime(df['Date']).dt.date
     df.sort_values(by='Date', inplace=True)
     return df
@@ -27,39 +41,30 @@ def PressTimePlot(df, selected_presses):
     df_filtered = df[df['Press'].isin(selected_presses)]
     for press, group in df_filtered.groupby('Press'):
         # Monthly average
-        # st.write([press, type(press)])
         Monthly = group.groupby('Date')['IsClickbait'].mean()
         Monthly.index = pd.to_datetime(Monthly.index)
         Monthly = Monthly.resample('M').mean()
-        Monthly = Monthly.rolling(10).mean()
-        fig.add_trace(px.line(Monthly, x=Monthly.index, y=Monthly.values, name=press).data[0])
+        Monthly = Monthly.rolling(4).mean()
+        # fig.add_trace(px.line(Monthly, x=Monthly.index, y=Monthly.values, hover_name=press).data[0])
+        fig.add_trace(px.line(Monthly, x=Monthly.index, y=Monthly.values).data[0])
+
     fig.update_layout(xaxis_title='Time (Monthly)',yaxis_title='Clickbait Ratio', legend_title='Press')
-            # Add a trace to the figure with the specified press as the legend group
     # Customize the layout
     return st.plotly_chart(fig)
 
 def CategoryTimePlot(df, selected_categories):
-    plt.figure(figsize=(8,6))
-    df_filtered = df[df['Press'].isin(selected_categories)]
+    fig = px.line(title='Clickbait Ratio: Category')
+    df_filtered = df[df['Category'].isin(selected_categories)]
     for category, group in df_filtered.groupby("Category"):
-        Monthly = group.groupby('Date')['IsClickbait'].mean().reset_index()
-        # print(type(Monthly))
-        Monthly = Monthly.rolling(30).mean()
-        plt.plot(Monthly, label=category, linestyle='--')
-    plt.xlabel('Time (Monthly)')
-    plt.ylabel('Clickbait Ratio')
-    plt.title('Clickbait Ratio: Category')
-    plt.legend()
-    return st.pyplot(plt)
-# def CategoryTimePlot(df, selected_categories):
-#     fig = px.line(title='Clickbait Ratio: Category')
-#     df_filtered = df[df['Press'].isin(selected_categories)]
-#     for category, group in df_filtered.groupby("Category"):
-#         Monthly = group[['Date','IsClickbait']].resample('M', on='Date').mean()['IsClickbait']
-#         Monthly = Monthly.rolling(10).mean()
-#         fig.add_trace(px.line(Monthly, x=Monthly.index, y=Monthly.values, name=category, line_dash='dash').data[0])
-#     fig.update_layout(xaxis_title='Time (Monthly)', yaxis_title='Clickbait Ratio', legend_title='Category')
-#     return st.plotly_chart(fig)
+        Monthly = group.groupby('Date')['IsClickbait'].mean()
+        Monthly.index = pd.to_datetime(Monthly.index)
+        Monthly = Monthly.resample('M').mean()
+        Monthly = Monthly.rolling(4).mean()
+        fig.add_trace(px.line(Monthly, x=Monthly.index, y=Monthly.values).data[0])
+        # next line cause error
+        # fig.add_trace(px.line(Monthly, x=Monthly.index, y=Monthly.values, name=category, line_dash='dash').data[0])
+    fig.update_layout(xaxis_title='Time (Monthly)', yaxis_title='Clickbait Ratio', legend_title='Category')
+    return st.plotly_chart(fig)
 
 # Load the data
 df = GetProcessedData('processed_data.csv')
@@ -78,16 +83,17 @@ def run():
             start_date = df['Date'].min()
         if not end_date:
             end_date = df['Date'].max()
+    st.title('新聞標題分析 Dashboard')
+    # Filter data based on selections
+    # Since the category,press effect each others, move the selection into plot function
+    filtered_df = SelectDate(df, start_date, end_date)
+    st.subheader('篩選日期後的資料格式')
+    st.write(filtered_df.head())
+
     t1, t2 = st.tabs(["Pingru", "Ding"])
     with t1:
-        # Streamlit app layout
-        st.title('新聞標題分析 Dashboard')
-        # Filter data based on selections
-        # Hide
         # mask = (df['Press'].isin(selected_press)) & (df['Date'] >= start_date) & (df['Date'] <= end_date)
         # filtered_df = df[mask]
-        filtered_df = SelectDate(df, start_date, end_date)
-        st.write(filtered_df.head())
 
         # Clickbait Ratio by Category Bar Chart
         st.header('各類別誘餌式標題比例')
@@ -113,9 +119,11 @@ def run():
         st.plotly_chart(fig_clickbait_press)
     with t2:
         st.header('新畫的圖')
+        st.write('遇到問題:無法用plotly.express標註每條線的hover_name,color等資訊。')
+        st.write("ValueError: Value of 'hover_name' is not the name of a column in 'data_frame'. Expected one of ['IsClickbait'] but received: ETToday")
+        st.write('error in line 48, in PressTimePlot')
         PressTimePlot(filtered_df, selected_presses)
-    # CategoryTimePlot(df, selected_categories)
-    # Clickbait Ratio by Press Bar Chart
+        CategoryTimePlot(filtered_df, selected_categories)
 
 if __name__ == "__main__":
-  run()
+    run()
